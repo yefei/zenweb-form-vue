c6b9c3ac547faf181fde79ab3e23e113dfc0724d
YeFei <316606233@qq.com>
2021-10-29 14:17:04 +0800

    readme

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,30 @@
       <el-col :span="8"><el-button type="primary" @click="openDialog">打开表单</el-button></el-col>
     </el-row>
 
+    <div style="margin-top:30px">
+      只需要一行代码即可完成表单渲染，所有表单项都由服务端返回，减轻前端重复工作。
+      <div class="code">
+        &lt;form-api url="<span v-text="url" />" @cancel="" @success="" /&gt;
+      </div>
+    </div>
+
     <el-dialog
       title="表单"
       :visible.sync="open">
-      <form-api v-if="open" :url="url" @cancel="open = false" @success="success" />
+      <form-api v-if="open" :url="url" @cancel="close" @success="success" />
     </el-dialog>
   </div>
 </template>
+
+<style>
+.code {
+  background-color: #eee;
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-top: 20px;
+  font-family: monospace;
+}
+</style>
 
 <script>
 import FormApi from './components/FormApi.vue'
@@ -33,6 +50,9 @@ export default {
       this.open = true;
     },
     success() {
+      this.open = false;
+    },
+    close() {
       this.open = false;
     }
   }
